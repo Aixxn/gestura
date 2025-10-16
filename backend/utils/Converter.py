c131b319f30b03ac73c070ca514e223bedfa1259
baronocasiones <baronocasiones@gmail.com>
2025-10-17 05:23:36 +0800

    Data pipeline mostly done, still need to connect to ai service.

--- backend/utils/Converter.py
+++ backend/utils/Converter.py
@@ -1,26 +1,64 @@
-from kafka import KafkaProducer, KafkaConsumer
-import json
+import mediapipe as mp
+import cv2 as cv
+import numpy as np
 
 
 class Converter:
     def __init__(self):
-        self.sequence = []
-        self.consumer = KafkaConsumer(
-                'raw-data',
-                bootstrap_servers=['localhost:9092'],
-                group_id='python_converter',
-                auto_offset_reset='earliest',
-                value_deserializer=lambda v: json.loads(v.decode('utf-8'))
-                )
-        self.producer = KafkaProducer(
-                bootstrap_servers=['localhost:9092'],
-                value_serializer=lambda v: json.dumps(v).encode('utf-8')
-                )
+        self.mp_model = mp.solutions.holistic.Holistic()
 
-        for mssg in self.consumer:
-            self.convert_data(mssg)
+    def point_detection(self, image_byte):
+        '''
+        args:
+            image_byte: image buffer (byte)
 
-    def convert_data(self, data):
-        pass
+        This method peform two processes, it fist converts image buffer (byte)
+        into a numpy array then, converts the numpy array into an image using
+        cv.imdecode(), essentially doing image byte to 2d image. It will then
+        add landmarks to the image using the mediapipe model's process() method
+        '''
+        nparr = np.frombuffer(image_byte, np.uint8)
+        cv_image = cv.imdecode(nparr, cv.IMREAD_COLOR)
+        image = cv.cvtColor(cv_image, cv.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = self.mp_model.process(image)
+        image.flags.writeable = True
+        self.image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+        return results
 
+    def extract_keypoints(self, results):
+        '''
+        args:
+            result: Image with keypoints
 
+        This method extracts the coordinates of each keypoints and turn it into 
+        a flatten numpy array.
+        '''
+        # list comprehension to loop over results and get needed data, then arranged to np.array. flattened to turn it into one array. else is to make a placeholder
+        lh = np.array([[res.x, res.y, res.z] for res in results
+                       .left_hand_landmarks
+                       .landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+
+        rh = np.array([[res.x, res.y, res.z] for res in results
+                       .right_hand_landmarks
+                       .landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+
+        face = np.array([[res.x, res.y, res.z] for res in results
+                         .face_landmarks
+                         .landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+
+        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results
+                         .pose_landmarks
+                         .landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+
+        # puts all data into one array
+        a = np.concatenate([face, lh, rh, pose])
+
+        lm_list = []
+
+        for lm in a:
+            base = a[0]
+            lm_list.append(lm - base)
+        lm_list = np.array(lm_list, dtype=np.float32).tolist()
+
+        return lm_list
---
+++ backend/utils/Pipeline.py
@@ -0,0 +1,93 @@
+from kafka import KafkaProducer, KafkaConsumer
+import json
+from Converter import Converter
+import threading
+import redis
+
+KAFKA_LOCAL_HOST_SERVER = 'localhost:9092'
+REDIS_PORT = 6739
+REDIS_HOST = 'localhost'
+NUM_PIPELINE_INSTANCE = 1
+
+
+class Pipeline:
+    def __init__(self, instance_id):
+        self.instance_id = instance_id
+        self.redis_client = redis.Redis(
+                host=REDIS_HOST,
+                port=REDIS_PORT,
+                decode_responses=True
+                )
+        self.converter = Converter()
+        self.consumer = KafkaConsumer(
+            'rawImageData',
+            bootstrap_servers=[KAFKA_LOCAL_HOST_SERVER],
+            group_id='image_to_vector',
+            auto_offset_reset='earliest'
+        )
+        self.producer = KafkaProducer(
+            bootstrap_servers=[KAFKA_LOCAL_HOST_SERVER],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
+    def _convert_data(self, data):
+        # but here, still don't know what kind of data type needed by the translation model
+        result = self.converter.point_detection(data)
+        keypoints = self.converter.extract_keypoints(result)
+        return keypoints
+
+    def start_consuming(self):
+        print(f'Pipeline {self.instance_id}: Listening for data...')
+        for mssg in self.consumer:
+            if mssg.key is None:
+                print(f'[{self.instance_id}] Skipping message: \
+                      No client ID (key) provided.')
+                continue
+
+            client_uuid = mssg.key.decode('utf-8')
+
+            if mssg.value.decode('utf-8') == 'stop':
+                self._produce_data(client_uuid)
+                self.redis_client.delete(client_uuid)
+                continue
+
+            keypoints = self._convert_data(mssg.value)
+            self.redis_client.rpush(client_uuid, json.dumps(keypoints))
+
+    def _produce_data(self, client_uuid):
+        vector_data_json_string = self.redis_client.lrange(client_uuid, 0, -1)
+        vector_data = [json.loads(item) for item in vector_data_json_string]
+        payload = {'keypoints': vector_data}
+        self.producer.send('keypoints',
+                           value=payload,
+                           key=client_uuid.encode('utf-8'),
+                           )
+        self.producer.flush()
+
+
+if __name__ == '__main__':
+    r = redis.Redis(REDIS_HOST, REDIS_PORT)
+    r.flushdb()
+    print('Redis flush for testing.')
+
+    threads = []
+
+    for i in range(NUM_PIPELINE_INSTANCE):
+        pipeline_instance = Pipeline(f'Worker-{i+1}')
+
+        thread = threading.Thread(target=pipeline_instance.start_consuming)
+        threads.append(thread)
+        thread.start()
+
+    try:
+        for thread in threads:
+            thread.join()
+
+    except KeyboardInterrupt:
+        print('\nShutting down pipelines...')
+
+
+
+
+
+
