c580f822b3fed05a341af2b093bd253ab05f419d
baronocasiones <baronocasiones@gmail.com>
2025-10-18 13:15:00 +0800

    Started implementing web socker server for translated sentence.

--- apiGateway/routes/api.ts
+++ apiGateway/routes/api.ts
@@ -1,21 +1,53 @@
 import express from "express";
 import { Kafka } from 'kafkajs';
 import multer from 'multer';
+import { WebSocketServer } from 'ws'
+import redis from 'redis'
 
 const router = express.Router();
 const kafka = new Kafka({ clientId: 'rawImageProducer', brokers: ['localhost:9092'] });
 const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'translatedSignConsumer' });
+const wss = new WebSocketServer({ port: 9898 });
+const redisClient = redis.createClient({ url: 'redis://localhost:6379' });
 
 const upload = multer();
 
+// redis
+redisClient.on('connect', () => console.log('Connected to redis'));
+redisClient.on('error', (e) => console.error('Redis client error:', e));
+
+// kafka
 (async () => {
     try {
         await producer.connect();
-        console.log('Kafka producer connected successfully')
+        console.log('Kafka producer connected successfully.')
     } catch (e) {
         console.error('Failed to connect Kafka producer:', e);
     }
 })();
+
+(async () => {
+    try{
+        await consumer.connect();
+        await consumer.subscribe({ topic: 'translatedSign', fromBeginning: true })
+        console.log('Kafka consumer connected successfully.');
+    } catch(e){
+        console.error('Failed to connect Kafka consumer.');
+    }
+});
+
+// TODO: continue here
+wss.on('connection', async (ws) => {
+    ws.on('message', (data) => {
+        console.log('Message received:', data);
+    });
+    await consumer.run({
+        eachMessage: async ({ message }) => {
+            ws.send(String(message));  //still not sure of the data that will be received
+        },
+    })
+});
 
 router.post('/stop', async (req, res) => {
     const uuid = req.body.uuid;
